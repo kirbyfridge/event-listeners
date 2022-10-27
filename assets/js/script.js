9e0cf0ed67b253904dbeacbd654945ce038d1d68
kirbyfridge <114848971+kirbyfridge@users.noreply.github.com>
2022-10-27 18:07:33 -0400

    increment & decrement buttons now function as intended

---
+++ assets/js/script.js
@@ -0,0 +1,14 @@
+var count = 0;
+var incrementEl = document.querySelector("#increment");
+var decrementEl = document.querySelector("#decrement");
+var countEl = document.querySelector("#count");
+
+function setCounterText() {
+  countEl.textContent = count;
+}
+
+// TODO: Add event listener to increment button
+incrementEl.addEventListener("click", function() {countEl.textContent++});
+
+// TODO: Add event listener to decrement button 
+decrementEl.addEventListener("click", function() {countEl.textContent--});
